2addf6ccc85e09d1c334845a33dc68a70e3bdf32
Geremy Giles <ggiles2@uccs.edu>
2025-10-02 13:12:37 -0600

    Prototyping the firing mechanic

--- GDD2100/Assets/FireBall.cs
+++ GDD2100/Assets/FireBall.cs
@@ -1,15 +1,53 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
     [SerializeField] GameObject football;
+    [SerializeField] GameObject cannon;
+    [SerializeField] float cooldown = 1.0f;
+
+    float activeCooldown = 0.0f;
+
+    private void FixedUpdate()
+    {
+        if (activeCooldown > 0)
+        {
+            activeCooldown -= Time.fixedDeltaTime;
+        }
+
+        if (activeCooldown < 0)
+        {
+            activeCooldown = 0;
+        }
+    }
 
     public void Fire()
     {
+        if (activeCooldown > 0)
+        {
+            return;
+        }
+
         Debug.Log("Football launched!");
 
-        GameObject ball = Instantiate(football, transform.position, gameObject.transform.rotation);
+        Quaternion cannonRotationQuat = cannon.transform.rotation;
+        Vector3 cannonRotation = new Vector3(cannonRotationQuat.eulerAngles.y, cannonRotationQuat.eulerAngles.z, cannonRotationQuat.eulerAngles.x);
 
-        ball.GetComponent<Rigidbody>().AddForce(transform.forward * 50, ForceMode.Impulse);
+        GameObject ball = Instantiate(football, transform.position, cannonRotationQuat);
+
+        ball.GetComponent<Rigidbody>().AddForce(Vector3.forward * 50, ForceMode.Impulse);
+
+        Debug.Log(cannonRotationQuat.eulerAngles);
+        Debug.Log(cannonRotation);
+
+        Debug.Log(Vector3.forward);
+
+        activeCooldown = cooldown;
+    }
+
+    IEnumerator Cooldown( float seconds )
+    {
+        yield return new WaitForSeconds(seconds);
     }
 }
