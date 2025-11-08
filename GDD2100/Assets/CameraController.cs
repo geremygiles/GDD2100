8cd5cff00b0bebd3f571a8f55ed71a722bc363c8
Geremy Giles <ggiles2@uccs.edu>
2025-11-07 22:57:36 -0700

    Correcting Instructions and Hiding Crosshair

--- GDD2100/Assets/CameraController.cs
+++ GDD2100/Assets/CameraController.cs
@@ -5,8 +5,10 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] CinemachineCamera standardCamera;
     [SerializeField] CinemachineCamera zoomedCamera;
+    [SerializeField] GameObject crosshair;
     public void ToggleZoom()
     {
         standardCamera.enabled = !standardCamera.enabled;
+        crosshair.SetActive(!standardCamera.enabled);
     }
 }
